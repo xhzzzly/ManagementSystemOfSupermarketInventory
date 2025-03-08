75ddbf4bf42e39a77207c0f758bb1d02280e0989
Tsun-Chi Wong <xhzzzly@126.com>
2025-03-08 16:20:25 +0800

    完成用户密码管理系统框架

---
+++ src/userPasswordManagement.c
@@ -0,0 +1,7 @@
+#include "userPasswordManagement.h"
+#include "config.h"
+
+void userPasswordManagement(void)
+{
+    // Code here
+}
---
+++ src/userPasswordManagement.h
@@ -0,0 +1,6 @@
+#ifndef USERPASSWORDMANAGEMENT_H
+#define USERPASSWORDMANAGEMENT_H
+
+void userPasswordManagement(void);
+
+#endif
