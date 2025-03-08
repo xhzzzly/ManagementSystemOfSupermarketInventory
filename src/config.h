77c4496c408e5e16ebcdd2d1ee84dc526d90b949
Tsun-Chi Wong <xhzzzly@126.com>
2025-03-08 15:33:20 +0800

    完成了main框架

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* 权限变量。默认为0，1代表游客，2代表普通员工，3代表管理员*/
+int permission = 0;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h" /* 配置文件，存着些全局变量 */
+#include "logInSystem.h" /* 登入系统 */
+#include "mainMenu.h" /* 主菜单 */
+
+/* 函数声明，其实现在main函数后 */
+void exitPrompt(void);
+
+int main(void)
+{
+	/* 登入功能 */
+	while (logInSystem() != 1) {
+		printf("\t\t是否继续尝试？输入0退出系统：");
+		int choice;
+		scanf("%d", &choice);
+		if (choice == 0) {
+			exitPrompt();
+			return 0;
+		}
+	}
+	
+	/* 进入主菜单 */
+	mainMenu();
+	
+	exitPrompt();
+	return 0;
+}
+
+/*
+ * 函数功能：系统退出提示语
+ * 参数：无
+ * 返回值：无
+*/
+void exitPrompt(void)
+{
+	system("cls");
+	printf("感谢您使用本超市库存管理系统。\n");
+	system("pause");
+}
