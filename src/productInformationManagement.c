30f09e5a1078e43713b213bdd6683bec167c33f5
Tsun-Chi Wong <xhzzzly@126.com>
2025-03-08 16:12:58 +0800

    完成了商品信息管理系统的框架

---
+++ src/productInformationManagement.c
@@ -0,0 +1,7 @@
+#include "productInformationManagement.h"
+#include "config.h"
+
+void productInformationManagement(void)
+{
+    // 请在此处添加商品信息管理的代码
+}
---
+++ src/productInformationManagement.h
@@ -0,0 +1,6 @@
+#ifndef PRODUCTINFORMATIONMANAGEMENT_H
+#define PRODUCTINFORMATIONMANAGEMENT_H
+
+extern void productInformationManagement(void);
+
+#endif
