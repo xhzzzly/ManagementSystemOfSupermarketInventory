9e46f8a0e700add58571ef66ef293e7142b53277
Tsun-Chi Wong <xhzzzly@126.com>
2025-03-08 16:24:10 +0800

    完成数据读存框架

---
+++ src/dataReadingAndStorage.c
@@ -0,0 +1,7 @@
+#include "dataReadingAndStorage.h"
+#include "config.h"
+
+void dataReadingAndStorage(void)
+{
+    // 在此处添加代码
+}
---
+++ src/dataReadingAndStorage.h
@@ -0,0 +1,6 @@
+#ifndef DATA_READING_AND_STORAGE_H
+#define DATA_READING_AND_STORAGE_H
+
+void readDataFromFile(void);
+
+#endif
