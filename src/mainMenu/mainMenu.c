db810281a3a85b74be95501a3680539d80e5d016
Tsun-Chi Wong <xhzzzly@126.com>
2025-03-11 21:25:12 +0800

    完善mainMenu

--- src/mainMenu/mainMenu.c
+++ src/mainMenu/mainMenu.c
@@ -1,4 +1,8 @@
 #include "permissionDegree.h"
+#include "productInformationManagement.h"
+#include "inventoryManagement.h"
+#include "userPasswordManagement.h"
+#include "dataReadingAndStorage.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +11,9 @@ void mainMenu(void)
 {
     while (1) {
         system("cls");
+
+        // 进入功能的权限判断留给子功能模块
+
         printf("\t\t   ---------------  \n");
         printf("\t\t|  超市库存管理系统  |\n");
         printf("\t\t|  1. 商品信息管理   |\n");
@@ -23,15 +30,19 @@ void mainMenu(void)
             return;
         case 1:
             /* 商品信息管理 */
+            productInformationManagement();
             break;
         case 2:
             /* 库存管理 */
+            inventoryManagement();
             break;
         case 3:
             /* 用户密码管理 */
+            userPasswordManagement();
             break;
         case 4:
             /* 数据读取与存储 */
+            dataReadingAndStorage();
             break;
         default:
             printf("\t\t输入错误，请重新输入\n");
